1c8721f615b265973d8c00afd0d8219b4a61afb5
mrq <mrq@ecker.tech>
2023-02-04 22:48:00 -0600

    Commit for 2023.02.04 22-48-51.7z

---
+++ bin/data/entities/gui/hud/scripts/hud.lua
@@ -0,0 +1,154 @@
+local scene = entities.currentScene()
+local controller = entities.controller()
+local metadata = ent:getComponent("Metadata")
+local masterdata = scene:getComponent("Metadata")
+
+local visorLayers = 5
+local children = {}
+for i=1, visorLayers do
+	children[i] = ent:loadChild("./overlay.json",true)
+end
+
+local soundEmitter = ent:loadChild("./sound.json",true)
+
+local timer = Timer.new()
+if not timer:running() then timer:start() end
+
+Static = {
+	values = {},
+	get = function( obj )
+		if obj == nil then
+			obj = scene
+		end
+		if Static.values[""..obj:uid()] == nil then
+			Static.values[""..obj:uid()] = {}
+		end
+		return Static.values[""..obj:uid()]
+	end
+}
+
+local lerper = {
+	to = Quaternion(0,0,0,1),
+	from = Quaternion(0,0,0,1),
+	a = 0
+}
+
+local rotate = function( delta )	
+	delta.x = delta.x * 0.1
+	delta.y = delta.y * 0.1
+
+	local transform = ent:getComponent("Transform")
+	local rotation = {
+		x = Quaternion.axisAngle( Vector3f(0, 1, 0), delta.x ),
+		y = Quaternion.axisAngle( Vector3f(1, 0, 0), delta.y )
+	}
+
+	lerper.a = 0
+	lerper.from = Quaternion.multiply( transform.orientation, rotation.x:multiply(rotation.y) )
+	transform.orientation = lerper.from
+	for k, obj in pairs(children) do
+		obj:getComponent("Transform").orientation = transform.orientation
+	end
+end
+
+local windowSize = masterdata["system"]["config"]["window"]["size"];
+local entTransform = ent:getComponent("Transform")
+entTransform.scale.x = entTransform.scale.x * windowSize.x / windowSize.y;
+
+for k, obj in pairs(children) do
+	local transform = obj:getComponent("Transform")
+	transform.scale = entTransform.scale;
+	transform.position.z = -0.5 + ((k-1) * 0.005)
+end
+
+ent:addHook( "window:Resized", function( payload )
+	if entTransform.scale.y == entTransform.scale.z then
+		entTransform.scale.x = entTransform.scale.y * payload["window"]["size"][1] / payload["window"]["size"][2];
+	end
+
+	for k, obj in pairs(children) do
+		local transform = obj:getComponent("Transform")
+		transform.scale = entTransform.scale;
+	end
+end )
+
+ent:addHook( "controller:Camera.Rotated", function( payload )
+	rotate( {
+		x = -payload.angle.yaw,
+		y = -payload.angle.pitch
+	})
+--[[
+	local transform = ent:getComponent("Transform")
+	lerper.a = 0
+	local counterOrientation = Quaternion( payload.delta[1], payload.delta[2], payload.delta[3], payload.delta[4] ):inverse()
+	lerper.from = Quaternion.multiply( transform.orientation, counterOrientation )
+	transform.orientation = lerper.from
+	for k, obj in pairs(children) do
+		obj:getComponent("Transform").orientation = transform.orientation
+	end
+]]
+end )
+--[[
+ent:addHook( "window:Mouse.Moved", function( payload )
+	if payload.invoker ~= "client" then return end
+
+	local delta = payload.mouse.delta
+	local size = payload.mouse.size
+
+	if delta == nil or size == nil then return end
+	if delta.x == 0 or delta.y == 0 then return end
+
+	delta.x = -delta.x / size.x 
+	delta.y = -delta.y / size.y 
+
+	rotate( delta )
+end )
+]]
+
+ent:bind( "tick", function(self)
+	for k, obj in pairs(children) do
+		local metadata = obj:getComponent("Metadata")
+		local glow = math.sin(time.current()) * 0.5 + 0.5 -- constrained to [0,1]
+		glow = glow * 0.2 + 0.65 -- constrained to [0.65, 0.85]
+		metadata["alpha"] = glow
+		obj:setComponent("Metadata", metadata)
+	end
+
+	local controllerTransform = controller:getComponent("Transform")
+	local controllerCamera = controller:getComponent("Camera")
+	local controllerCameraTransform = controllerCamera:getTransform()
+	local transform = ent:getComponent("Transform")
+	
+	local speed = 2.5
+	if lerper.a == 1 then return end
+	lerper.a = lerper.a + time.delta() * speed
+	if lerper.a > 1 then lerper.a = 1 end
+
+	transform.orientation = lerper.from:slerp( lerper.to, lerper.a )
+	local orientation = transform.orientation
+	for k, obj in pairs(children) do
+		local transform = obj:getComponent("Transform")
+		transform.orientation = orientation
+		transform.model = controllerCamera:getProjection() * Matrix4f.translate( transform.position ) * transform.orientation:matrix() * Matrix4f.scale( transform.scale ) --Matrix4f.scale( Vector3f( 1.7776 * 2, 2, 2 ) )
+	end
+end )
+
+--[[
+	controller:callHook( "object:UpdateMetadata.%UID%", {
+		path = "overlay.position",
+		value = {
+			[1] = position.x,
+			[2] = position.y,
+			[3] = position.z
+		}
+	} )
+	controller:callHook( "object:UpdateMetadata.%UID%", {
+		path = "overlay.orientation",
+		value = {
+			[1] = orientation.x,
+			[2] = orientation.y,
+			[3] = orientation.z,
+			[4] = orientation.w
+		}
+	} )
+]]
---
+++ bin/data/entities/scripts/player.lua
@@ -0,0 +1,241 @@
+local ent = ent
+local scene = entities.currentScene()
+local metadata = ent:getComponent("Metadata")
+local transform = ent:getComponent("Transform")
+local physicsState = ent:getComponent("PhysicsState")
+local camera = ent:getComponent("Camera")
+local cameraTransform = camera:getTransform()
+
+-- setup all timers
+local timers = {
+	use = Timer.new(),
+	holp = Timer.new(),
+	flashlight = Timer.new(),
+	physcannon = Timer.new()
+}
+if not timers.use:running() then timers.use:start(); end
+if not timers.holp:running() then timers.holp:start(); end
+if not timers.flashlight:running() then timers.flashlight:start(); end
+if not timers.physcannon:running() then timers.physcannon:start(); end
+
+-- setup held object locals
+local heldObject = {
+	uid = 0,
+	distance = 0,
+	smoothSpeed = 4,
+	scrollSpeed = 16,
+	momentum = Vector3f(0,0,0),
+	rotate = false,
+}
+-- setup light locals
+local light = {
+	entity = nil
+}
+for k, v in pairs(ent:getChildren()) do
+	if v:name() == "Light" then
+		light.entity = v
+	end
+end
+
+if light.entity == nil then
+	light.entity = ent:loadChild("./playerLight.json",true)
+end
+light.metadata = light.entity:getComponent("Metadata")
+light.transform = light.entity:getComponent("Transform")
+light.power = light.metadata["light"]["power"]
+light.origin = Vector3f(light.transform.position)
+light.entity:setComponent("Metadata", { light = { power = 0 } })
+
+-- sound emitter
+local playSound = function( key, loop )
+	if not loop then loop = false end
+	local url = "/ui/" .. key .. ".ogg"
+	ent:callHook("sound:Emit.%UID%", {
+		filename = string.resolveURI(url, metadata["system"]["root"]),
+		spatial = true,
+		streamed = true,
+		volume = "sfx",
+		loop = loop
+	}, 0)
+end
+local stopSound = function( key )
+	local url = "/ui/" .. key .. ".ogg"
+	ent:callHook("sound:Stop.%UID%", {
+		filename = string.resolveURI(url, metadata["system"]["root"])
+	}, 0)
+end
+
+local useDistance = 6
+local pullDistance = useDistance * 4
+
+-- on tick
+ent:bind( "tick", function(self)
+	-- eye transform
+	local flattenedTransform = cameraTransform:flatten()
+	flattenedTransform.forward = ( transform.forward + Vector3f( 0, cameraTransform.forward.y, 0 ) ):normalize();
+
+	-- toggle flashlight
+	if light.enabled then
+		local center = flattenedTransform.position
+		local direction = flattenedTransform.forward * 8
+
+		local offset = 0.25
+		local _, depth = physicsState:rayCast( center, direction )
+		if depth >= 0.5 then
+			depth = 0.5
+		elseif depth < 0 then
+			depth = 0.5
+		end
+		light.transform.position = center + direction * (depth - offset)
+	end
+
+	if timers.flashlight:elapsed() > 0.5 and inputs.key("F") then
+		timers.flashlight:reset()
+
+		local metadata = { light = { power = light.power } }
+		if light.entity:getComponent("Metadata")["light"]["power"] ~= light.power then
+			metadata["light"]["power"] = light.power
+			light.enabled = true
+		else
+			metadata["light"]["power"] = 0
+			light.enabled = false
+		end
+		light.entity:setComponent("Metadata", metadata)
+
+		playSound("flashlight")
+	end
+
+	-- fire use ray
+	if timers.use:elapsed() > 0.5 and (inputs.key("E") or inputs.key("R_Y")) then
+		timers.use:reset()
+
+		local center = flattenedTransform.position
+		local direction = flattenedTransform.forward * useDistance
+
+		local prop, depth = physicsState:rayCast( center, direction )
+		local payload = {
+			user = ent:uid(),
+			uid = prop and prop:uid() or 0,
+			depth = depth,
+		}
+		if prop then
+			prop:lazyCallHook("entity:Use.%UID%", payload)
+		end
+		ent:lazyCallHook("entity:Use.%UID%", payload)
+	end
+
+	-- update HOLP
+	if heldObject.uid == 0 then
+		local mouse2 = inputs.key("Mouse2") or inputs.key("R_TRIGGER");
+		if mouse2 then
+		--[[
+			local center = transform.position + cameraTransform.position
+			local direction = transform.forward + Vector3f( 0, cameraTransform.forward.y, 0 )
+			direction = direction:normalize() * 4
+		]]
+			local center = flattenedTransform.position
+			local direction = flattenedTransform.forward * pullDistance
+			local prop, depth = physicsState:rayCast( center, direction )
+			if depth >= 0 and prop and not string.matched( prop:name(), "/^worldspawn/" ) then
+				local heldObjectTransform = prop:getComponent("Transform")
+				local heldObjectPhysicsState = prop:getComponent("PhysicsState")
+
+				local strength = 500
+				local distanceSquared = (heldObjectTransform.position - flattenedTransform.position):magnitude()
+
+				heldObjectPhysicsState:applyImpulse( flattenedTransform.forward * -heldObjectPhysicsState:getMass() * strength / distanceSquared )
+				if timers.physcannon:elapsed() > 1.0 then
+					timers.physcannon:reset()
+
+					playSound("phys_tooHeavy")
+				end
+			end
+		end
+	else
+		local mouse1 = inputs.key("Mouse1") or inputs.key("L_TRIGGER");
+		local mouse3 = inputs.key("Mouse3");
+		local wheel = inputs.analog("MouseWheel")
+
+		if wheel ~= 0 then
+			heldObject.distance = heldObject.distance + (wheel / 120 * heldObject.scrollSpeed) * time.delta()
+		end
+		if mouse3 then
+			heldObject.rotate = not heldObject.rotate
+		end
+
+		local prop = entities.get( heldObject.uid )
+		local heldObjectTransform = prop:getComponent("Transform")
+		local heldObjectPhysicsState = prop:getComponent("PhysicsState")
+
+		if mouse1 and timers.physcannon:elapsed() > 0.5 then
+			timers.physcannon:reset()
+
+			heldObject.uid = 0
+			heldObjectPhysicsState:enableGravity(true)
+			heldObjectPhysicsState:applyImpulse( flattenedTransform.forward * heldObjectPhysicsState:getMass() * 1000 )
+
+			playSound("phys_launch"..math.random(1,4))
+		else
+			if heldObject.rotate then
+				heldObjectTransform.orientation = Quaternion.lookAt( (heldObjectTransform.position - flattenedTransform.position):normalize(), transform.up )
+			end
+			
+			local forward = flattenedTransform.forward * heldObject.distance --flattenedTransform.orientation:rotate( Vector3f(0,0,1) )
+			if heldObject.smoothSpeed ~= 0 then
+				local target = flattenedTransform.position + forward
+				local offset = target - heldObjectTransform.position
+				local delta = offset * time.delta() * heldObject.smoothSpeed
+
+				local distance = delta:norm()
+				if distance > 0.001 then
+					if timers.holp:elapsed() > 0.125 then
+						timers.holp:reset()
+						heldObjectPhysicsState:setVelocity( delta * 20 )
+					end
+				else
+					heldObjectPhysicsState:setVelocity( Vector3f(0,0,0) )
+				end
+			else
+				heldObjectTransform.position = flattenedTransform.position + forward
+			end
+		end
+	end
+end )
+-- on use
+ent:addHook( "entity:Use.%UID%", function( payload )
+	if payload.user ~= ent:uid() then return end
+
+	local validUse = false
+	
+	if heldObject.uid == 0 and payload.depth > 0 then
+		local prop = entities.get( payload.uid )
+		local propMetadata = prop:getComponent("Metadata")
+		if propMetadata["holdable"] then
+			validUse = true
+			local offset = transform.position - prop:getComponent("Transform").position
+
+			heldObject.uid = payload.uid
+			heldObject.distance = offset:norm()
+		
+			prop:getComponent("PhysicsState"):enableGravity(false)
+		else
+			validUse = not string.matched( prop:name(), "/^worldspawn/" )
+		end
+	elseif heldObject.uid ~= 0 then
+		validUse = true
+		local prop = entities.get( heldObject.uid )
+		local heldObjectPhysicsState = prop:getComponent("PhysicsState")
+		heldObjectPhysicsState:enableGravity(true)
+		heldObjectPhysicsState:applyImpulse( heldObject.momentum )
+		
+		heldObject.uid = 0
+		heldObject.distance = 0
+		heldObject.momentum = Vector3f(0,0,0)
+	end
+
+	if validUse then
+		playSound("select")
+	else
+		playSound("deny")
+	end
+end )
